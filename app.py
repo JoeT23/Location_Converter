a10869abb72301e958ac6ca8ae0e958dea041125
JoeT23 <joe.peterborough@gmail.com>
2026-06-19 08:06:35 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+# simplify UI with integrated converter py
 import gradio as gr
-from converter import easting_northing_to_latlon, gridref_to_en
+from converter import convert
 
 with gr.Blocks() as demo:
 
@@ -24,30 +25,11 @@ with gr.Blocks() as demo:
             btn = gr.Button("Convert")
             output_text = gr.Textbox(label="Results")
 
-            def convert_ui(mode, easting, northing, gridref):
-
-                if mode == "Easting / Northing":
-
-                    if easting is None or northing is None:
-                        return "Please enter values."
-
-                    lat, lon = easting_northing_to_latlon(float(easting), float(northing))
-                    return f"Latitude: {lat:.6f}, Longitude: {lon:.6f}"
-
-                else:
-
-                    result = gridref_to_en(gridref)
-
-                    if result is None:
-                        return "Invalid grid reference"
-
-                    e, n = result
-                    lat, lon = easting_northing_to_latlon(e, n)
-
-                    return f"Latitude: {lat:.6f}, Longitude: {lon:.6f}"
+            def run_convert(mode, easting, northing, gridref):
+                return convert(mode, easting, northing, gridref)
 
             btn.click(
-                fn=convert_ui,
+                fn=run_convert,
                 inputs=[mode, easting, northing, gridref],
                 outputs=output_text
             )
